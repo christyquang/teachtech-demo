45d313586fb348141b13b8723dcad578870beef0
Christy Quang <christyquang@berkeley.edu>
2024-04-29 14:05:37 -0700

    updated upload files section

--- src/navigation.js
+++ src/navigation.js
@@ -32,10 +32,6 @@ export const headerData = {
           href: getPermalink('/#features'),
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
           text: 'Pricing',
           href: getPermalink('/pricing'),
         },
@@ -44,16 +40,8 @@ export const headerData = {
           href: getPermalink('/about'),
         },
         {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
           text: 'Terms',
           href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy Policy',
-          href: getPermalink('/privacy'),
         },
       ],
     },
@@ -122,48 +110,20 @@ export const headerData = {
 
 export const footerData = {
   links: [
+    {},
+    {},
     {
       title: 'Product',
       links: [
         { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
         { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
+        { text: 'Testimonials', href: '#' },
       ],
     },
     {
       title: 'Platform',
       links: [
-        { text: 'Developer API', href: '#' },
         { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
       ],
     },
   ],
